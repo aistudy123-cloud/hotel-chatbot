003f725d9bab06f0e71552b91f77f20918fbaea2
aistudy123-cloud <koenigstein@tu-berlin.de>
2025-11-23 23:30:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -277,10 +277,10 @@ if user_msg:
             displayed += ch
             output.markdown(displayed)
             time.sleep(random.uniform(0.01, 0.03))
-#      output.markdown(bot_text)
+        output.empty()
         
     st.session_state.history.append(("assistant", bot_text))
-#    st.rerun()
+    st.rerun()
     log_event(user_msg, picked_id, sim if best is not None else 0.0)
 import gspread
 from google.oauth2.service_account import Credentials
@@ -325,6 +325,7 @@ def log_event_to_gsheet(timestamp_iso: str, user_text: str, picked_id: str, simi
         # ws.update("A1:E1", [["timestamp", "user_text", "picked_id", "similarity", "session_id"]])
         row.append(session_id)
     ws.append_row(row, value_input_option="USER_ENTERED")
+
 
 
 
